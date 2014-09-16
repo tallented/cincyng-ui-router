5ce932faf72efbd528e742bfb0fee4cc26a56aca
Chris Tallent <chris@tallented.net>
2014-09-16 09:09:51 -0400

    Fix bugs

--- app/js/controllers/login_controller.js
+++ app/js/controllers/login_controller.js
@@ -8,6 +8,10 @@ angular.module("app").controller('LoginController', function($rootScope, $scope,
     // Fake the login and set the username on the security context
     SecurityContext.login($scope.credentials.username);
 
+    // Check for a pre-login state
+    // This must be done before closing the modal which might cause a state change
+    var preLoginState = $rootScope.preLoginState;
+
     // Close the login modal
     $scope.$close();
 
@@ -15,8 +19,6 @@ angular.module("app").controller('LoginController', function($rootScope, $scope,
     // otherwise head back to the home page
     // The $timeout is used with a time of zero to execute this check immediately after the modal is closed
     $timeout(function() {
-      // Check for a pre-login state
-      var preLoginState = $rootScope.preLoginState;
       if (preLoginState) {
         console.log('Found pre login state ' + preLoginState.toState);
         $state.go(preLoginState.toState, preLoginState.toParams);
